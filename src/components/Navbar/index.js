2e2fd778790b2dccbf810df489749f790c41048c
Marcus Chua <marcuschua@berkeley.edu>
2024-09-20 00:17:26 -0700

    Edited Mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -34,23 +34,28 @@ const Navbar = () => {
         </ButtonContainer>
         {
           isOpen &&
+          //MOBILE STUFF HERE
           <MobileMenu isOpen={isOpen}>
             <MobileLink href="#about" onClick={() => {
               setIsOpen(!isOpen)
             }}>About</MobileLink>
+            {/*}
             <MobileLink href='#skills' onClick={() => {
               setIsOpen(!isOpen)
             }}>Skills</MobileLink>
             <MobileLink href='#experience' onClick={() => {
               setIsOpen(!isOpen)
             }}>Experience</MobileLink>
+          */}
             <MobileLink href='#projects' onClick={() => {
               setIsOpen(!isOpen)
-            }}>Projects</MobileLink>
+            }}>Writing</MobileLink>
+            {/*}
             <MobileLink href='#education' onClick={() => {
               setIsOpen(!isOpen)
             }}>Education</MobileLink>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          */}
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.linkedin} target="_blank">Linkedin Profile</GitHubButton>
           </MobileMenu>
         }
       </NavbarContainer>
